c3296211ecf360c1c93b6a9817489cd96f8f0170
Angela Wang <angelavveivvei@gmail.com>
2023-12-12 13:35:50 -0500

    uploaded first file

--- Java/Introduction/FirstJavaProgram.java
+++ Java/Introduction/FirstJavaProgram.java
@@ -1,17 +1,7 @@
 package Java.Introduction;
 
-import java.util.Scanner;
-
 public class FirstJavaProgram {
     public static void main(String[] args) {
-        
-        try (Scanner scan = new Scanner(System.in)) {
-            int num1; 
-            
-            System.out.println("Please enter a number: ");    
-            num1 = scan.nextInt();
-            
-            System.out.println("twice your number is: " + num1 * 2);
-        }
+        System.out.println(5/2.0);
     }
 }
