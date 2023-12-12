52aa49123a1e2fe7a6b8881f3b5910b822538d41
Angela Wang <angelavveivvei@gmail.com>
2023-12-12 13:36:30 -0500

    finished uploading all introduction files

---
+++ Java/Introduction/SecondJavaProgram.java
@@ -0,0 +1,17 @@
+package Java.Introduction;
+
+import java.util.Scanner;
+
+public class SecondJavaProgram {
+    public static void main(String[] args) {
+        
+        try (Scanner scan = new Scanner(System.in)) {
+            int num1; 
+            
+            System.out.println("Please enter a number: ");    
+            num1 = scan.nextInt();
+            
+            System.out.println("twice your number is: " + num1 * 2);
+        }
+    }
+}
